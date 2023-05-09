11a6d2662383c95f6635affa02a8a6872ed99b7f
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2023-05-09 16:29:45 +0200

    fixed name

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_search_service" {
+resource "azurerm_search_service" "search_service" {
   name                = var.name
   location            = var.location
   resource_group_name = var.rg_name
