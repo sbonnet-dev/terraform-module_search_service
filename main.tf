1595072522f085a4fc82288cdbcbff62f9e9d2a6
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2023-05-10 11:16:56 +0200

    changed naming convention

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_search_service" "search_service" {
-  name                = var.name
+  name     = lower(format("ss-%s-%s-%s", var.project, var.environment, var.name))
   location            = var.location
   resource_group_name = var.rg_name
   sku                 = "standard"
